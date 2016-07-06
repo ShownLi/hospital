8a87abed7613cac69b295068fc7ae120c0a17a16
lianzheng <lz@tourmade.com>
2016-07-06 10:57:10 +0800

    lz

--- src/com/tourmade/crm/action/CaseController.java
+++ src/com/tourmade/crm/action/CaseController.java
@@ -49,9 +49,9 @@ public class CaseController extends BaseSimpleFormController {
 	@RequestMapping(value = "/list.do",produces="application/json;charset=utf-8")
 	@ResponseBody
 	public String queryData(HttpServletRequest request, HttpSession session, Model model, DemoCase crmcase, PageHelper page) {
-
-		//System.out.println(customerid);
+	
 		QueryResult<DemoCase> r = service.queryCase(crmcase, page, request);
+		//System.out.println(crmcase);
 		String result = JSONUtilS.object2json(r);
 
 		return result;
@@ -106,6 +106,48 @@ public class CaseController extends BaseSimpleFormController {
 
 	@RequestMapping(value = "/addfromcustomer.html", method = { RequestMethod.POST, RequestMethod.GET })
 	public String addfromcustomer(Model model, int customerid) {
+		String country = "country";
+		String language = "case.language";
+		String contacttype = "case.contacttype";
+		String withwho = "case.withwho";
+		String hotel = "case.hotel";
+		String meals = "case.meals";
+		String guide = "case.guide";
+		String destination = "destination";
+		String source = "case.source";
+		List<DemoList> v1 = service.getParameterInfo(country);
+		List<DemoList> v2 = service.getParameterInfo(language);
+		List<DemoList> v3 = service.getParameterInfo(contacttype);
+		List<DemoList> v4 = service.getParameterInfo(withwho);
+		List<DemoList> v5 = service.getParameterInfo(hotel);
+		List<DemoList> v6 = service.getParameterInfo(meals);
+		List<DemoList> v7 = service.getParameterInfo(guide);
+		List<DemoList> v8 = service.getParameterInfo(destination);
+		List<DemoList> v9 = service.getParameterInfo(source);
+		List<DemoList> customer = service.getCustomer();
+		List<DemoList> sales = service.getSales();
+		JSONArray countryresult = JSONArray.fromObject(v1);
+		JSONArray languageresult = JSONArray.fromObject(v2);
+		JSONArray contactresult = JSONArray.fromObject(v3);
+		JSONArray withresult = JSONArray.fromObject(v4);
+		JSONArray hotelresult = JSONArray.fromObject(v5);
+		JSONArray mealsresult = JSONArray.fromObject(v6);
+		JSONArray guideresult = JSONArray.fromObject(v7);
+		JSONArray destinationresult = JSONArray.fromObject(v8);
+		JSONArray sourceresult = JSONArray.fromObject(v9);
+		JSONArray customerresult = JSONArray.fromObject(customer);
+		JSONArray salesresult = JSONArray.fromObject(sales);
+		model.addAttribute("country",countryresult);
+		model.addAttribute("language",languageresult);
+		model.addAttribute("contacttype",contactresult);
+		model.addAttribute("withwho",withresult);
+		model.addAttribute("hotel",hotelresult);
+		model.addAttribute("meals",mealsresult);
+		model.addAttribute("guide",guideresult);
+		model.addAttribute("destination",destinationresult);
+		model.addAttribute("source",sourceresult);
+		model.addAttribute("customer",customerresult);
+		model.addAttribute("sales",salesresult);
 		model.addAttribute("customerid",customerid);
 		return "/case/add";
 	}
