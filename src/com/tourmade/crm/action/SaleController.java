b760587ea2f7e2a914365e4f63d16288e00fb5d7
ShownLi <lxy@tourmade.com>
2017-02-07 15:55:03 +0800

    x

--- src/com/tourmade/crm/action/SaleController.java
+++ src/com/tourmade/crm/action/SaleController.java
@@ -67,9 +67,9 @@ public class SaleController extends BaseSimpleFormController {
 		
 		try {
 			String photoName = "photo"+sale.getAgencyId()+sale.getSalesId()+photo.getOriginalFilename();  
-			String photoPath = request.getSession().getServletContext().getRealPath("/WEB-INF/view/attachment/")+photoName;
+			String photoPath = request.getSession().getServletContext().getRealPath("/WEB-INF/view/attachment")+File.separator+photoName;
 			String cardName = "namecard"+sale.getAgencyId()+sale.getSalesId()+namecard.getOriginalFilename();  
-			String cardPath = request.getSession().getServletContext().getRealPath("/WEB-INF/view/attachment/")+cardName;
+			String cardPath = request.getSession().getServletContext().getRealPath("/WEB-INF/view/attachment")+File.separator+cardName;
 			File photoFile = new File(photoPath);  
 			File cardFile = new File(cardPath);  
 	        if (!photoFile.getParentFile().exists()) {  
